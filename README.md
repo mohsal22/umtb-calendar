49d738193537479e7ab2bac60b16723402439ae6
“mohsal22” <mohsal.petnovations@gmail.com>
2024-10-20 16:26:06 +0300

    added custom header

---
+++ README.md
@@ -0,0 +1,1 @@
+# umtb-calendar
---
+++ index.ts
@@ -0,0 +1,5 @@
+import DateTimePicker from "./src/DateTimePicker";
+import type { DateType, ModeType } from "./src/types";
+
+export type { DateType, ModeType };
+export default DateTimePicker;
